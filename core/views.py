b9b7aa723d15b5283f0773a47881d234e26c9c83
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-07-08 09:13:04 -0300

    drf - categorias

--- core/views.py
+++ core/views.py
@@ -1,2 +1,9 @@
-from django.shortcuts import render
+from unicodedata import category
+from rest_framework.viewsets import ModelViewSet
 
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
+
+class CategoriaViewset(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
