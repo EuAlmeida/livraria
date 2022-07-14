d4761c211200372f5a05fadd381149e22d46f1b3
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-07-14 15:12:08 -0300

    UEPA

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,30 @@
+from dataclasses import field
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Categoria 
+from core.models import Categoria, Livro 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
-        
+
+
+from core.models import Editora
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = "__all__"
+
+from core.models import Autor
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
 
--- livraria/urls.py
+++ livraria/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from core.views import CategoriaViewset
+from core.serializers import AutorSerializer
+from core.views import AutorViewSet, CategoriaViewset, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewset)
-
+router.register(r'editora', EditoraViewSet)
+router.register(r'autor', AutorViewSet)
+router.register(r'Livro', LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
